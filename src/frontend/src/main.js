99c2a0968e27791b9bccedd6890d135a1372f4cd
Dmytro <deepdows@gmail.com>
2022-08-07 03:28:40 +0300

    adding element ui

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueApexCharts from 'vue3-apexcharts'
+import installElementPlus from './plugins/element'
 
-createApp(App).use(router).use(VueApexCharts).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(router).use(VueApexCharts).mount('#app')
